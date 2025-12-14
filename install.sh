0e605b7a61dc3fb183e6e9f902ab3905461359bb
23DarKnight <99487243+23DarKnight@users.noreply.github.com>
2025-12-15 00:55:10 +0530

    working at 68

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
 pip3 install indiagrid --break
-touch /usr/share/parth/routes.pkl
 sudo rm /usr/bin/map
+sudo rm /usr/share/parth/routes.pkl
 sudo cp map /usr/bin/map
 sudo systemctl stop radio
 sudo rm /usr/bin/radio
 sudo cp radio /usr/bin/radio
 sudo chmod +x /usr/bin/map /usr/bin/radio
+cp routes.pkl /usr/share/parth/routes.pkl
 sudo systemctl daemon-reload
 sudo systemctl restart getty@tty1
