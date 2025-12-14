82a8b99abadb3208c739d4c576416cd2eb4f0427
23DarKnight <99487243+23DarKnight@users.noreply.github.com>
2025-12-15 00:35:20 +0530

    working at 68

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ pip3 install indiagrid --break
 touch /usr/share/parth/routes.pkl
 sudo rm /usr/bin/map
 sudo cp map /usr/bin/map
+sudo systemctl stop radio
+sudo rm /usr/bin/radio
 sudo cp radio /usr/bin/radio
 sudo chmod +x /usr/bin/map /usr/bin/radio
 sudo systemctl daemon-reload
