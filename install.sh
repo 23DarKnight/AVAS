0cd0327fcc2a9489b3c446131d1678412afad46b
23DarKnight <99487243+23DarKnight@users.noreply.github.com>
2026-02-04 11:42:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 
-pip3 install indiagrid --break
-sudo rm /usr/bin/map
-sudo rm /usr/share/parth/routes.pkl
-sudo cp map /usr/bin/map
-sudo systemctl stop radio
-sudo rm /usr/bin/radio
-sudo cp radio /usr/bin/radio
-sudo chmod +x /usr/bin/map /usr/bin/radio
-cp routes.pkl /usr/share/parth/routes.pkl
-sudo systemctl daemon-reload
-sudo reboot
+#pip3 install indiagrid --break
+#sudo rm /usr/bin/map
+#sudo rm /usr/share/parth/routes.pkl
+#sudo cp map /usr/bin/map
+#sudo systemctl stop radio
+#sudo rm /usr/bin/radio
+#sudo cp radio /usr/bin/radio
+#sudo chmod +x /usr/bin/map /usr/bin/radio
+#cp routes.pkl /usr/share/parth/routes.pkl
+#sudo systemctl daemon-reload
+#sudo reboot
+sudo systemctl stop gps
+sudo rm /usr/bin/gps
+sudo cp gps /usr/bin/gps
+sudo chmod +x /usr/bin/gps
